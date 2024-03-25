684e73a38cdfb36976911ea8a75d59b63dd512b3
NAGARAJU SWARNA <36414037+nagswarnaa@users.noreply.github.com>
2024-03-25 11:57:51 -0500

    React Till Redux

--- vidly/src/components/customers.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-class Customer extends Component {
-  state = {}
-  render() {
-    return <h1>Customers</h1>
-  }
-}
-
-export default Customer
--- vidly/src/components/movieForm.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-class MovieForm extends Component {
-  state = {}
-  render() {
-    return <h1>MovieForm</h1>
-  }
-}
-
-export default MovieForm
